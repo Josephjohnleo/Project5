f2d6b520d2b40e3d7b48104cebbc979d791de787
Josephjohnleo <josephjohnleoindia@gmail.com>
2020-07-19 01:01:37 -0700

    main is added

--- src/test/java/org/datas/Test1.java
+++ src/test/java/org/datas/Test1.java
@@ -1,6 +1,16 @@
 package org.datas;
 
 public class Test1 {
+	public void tes() {
+		System.out.println("enter the text");
+		int a,b,c,d;
+		a=10;
+		b=20;
+		c=30;
+		d=40;
+		int z= a+b+c+d;
+		System.out.println("the sum of value is "+z);
+	}
 public static void main(String[] args) {
 	System.out.println("Ave Maria");
 }
